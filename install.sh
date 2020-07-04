88dabac20b3de8be6d501bd01b9ceec4d1a43cf1
AJ Prasad <66101222+antaresx8@users.noreply.github.com>
2020-07-03 23:14:20 -0400

    mkdir -p ~/.cabal/bin

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ cd ~/Downloads
 wget https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
 tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
 rm cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz cabal.sig
+mkdir -p ~/.cabal/bin
 mkdir -p ~/.local/bin
 mv cabal ~/.local/bin/
 
